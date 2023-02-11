26d9699e46e0e87887e12f0052b009612b53b4e4
WandyS <wandy.saputra@hotmail.com>
2023-02-11 11:56:19 +0800

    Add Book and Author entities

---
+++ EFCoreMigrationNet7SqlLite/Entities/Author.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreMigrationNet7SqlLite.Entities;
+
+[Table("Authors")]
+public class Author
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(150)]
+    public string FirstName { get; set; }
+    
+    [Required]
+    [MaxLength(150)]
+    public string LastName { get; set; }
+
+    public Author(Guid id, string firstName, string lastName)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
---
+++ EFCoreMigrationNet7SqlLite/Entities/Book.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreMigrationNet7SqlLite.Entities;
+
+[Table("Books")]
+public class Book
+{
+    [Key]
+    public Guid Id { get; set; }
+    [Required]
+    [MaxLength(150)]
+    public string Title { get; set; }
+    [MaxLength(2500)]
+    public string? Description { get; set; }
+    public Guid AuthorId { get; set; }
+    public Author Author { get; set; } = null!;
+
+    public Book(Guid id, Guid authorId, string title, string? description)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        AuthorId = authorId;
+    }
+}
