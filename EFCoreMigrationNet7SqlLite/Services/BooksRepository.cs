5f816eb50fe82270a2478ecbd277bb42bcd1b4ad
WandyS <wandy.saputra@hotmail.com>
2023-02-11 12:15:32 +0800

    add BooksRepository

---
+++ EFCoreMigrationNet7SqlLite/Services/BooksRepository.cs
@@ -0,0 +1,42 @@
+﻿using EFCoreMigrationNet7SqlLite.DbContexts;
+using EFCoreMigrationNet7SqlLite.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMigrationNet7SqlLite.Services;
+
+public class BooksRepository : IBooksRepository
+{
+    private readonly BooksContext _booksContext;
+
+    public BooksRepository(BooksContext booksContext)
+    {
+        _booksContext = booksContext;
+    }
+    public IEnumerable<Book> GetBooks()
+    {
+        return _booksContext.Books
+            .Include(i => i.Author)
+            .ToList();
+    }
+
+    public Book? GetBook(Guid id)
+    {
+        return _booksContext.Books
+            .Include(i => i.Author)
+            .FirstOrDefault(f => f.Id == id);
+    }
+
+    public async Task<IEnumerable<Book>> GetBooksAsync()
+    {
+        return await _booksContext.Books
+            .Include(i => i.Author)
+            .ToListAsync();
+    }
+
+    public async Task<Book?> GetBookAsync(Guid id)
+    {
+        return await _booksContext.Books
+            .Include(i => i.Author)
+            .FirstOrDefaultAsync(f => f.Id == id);
+    }
+}
---
+++ EFCoreMigrationNet7SqlLite/Services/IBooksRepository.cs
@@ -0,0 +1,12 @@
+﻿using EFCoreMigrationNet7SqlLite.Entities;
+
+namespace EFCoreMigrationNet7SqlLite.Services;
+
+public interface IBooksRepository
+{
+    IEnumerable<Book> GetBooks();
+    Book? GetBook(Guid id);
+    
+    Task<IEnumerable<Book>> GetBooksAsync();
+    Task<Book?> GetBookAsync(Guid id);
+}
